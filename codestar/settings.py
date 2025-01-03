7b04d6fdba15190c4b1ca003846ebcca3dd8ff40
Graham Maher <mahergrahamsj@gmail.com>
2025-01-03 19:47:06 +0000

    add comment class

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-grahammaher-djangoblogw-9z26v1cl7tg.ws-eu117.gitpod.io','.herokuapp.com']
 
